47d8e443b4c881c6993470d727d0ce6bdc98ba4d
Nikhil Suwalka <suwalka18@gmail.com>
2021-10-30 13:08:38 +0530

    Created models

--- mainapp/forms.py
+++ mainapp/forms.py
@@ -1,11 +1,15 @@
 from django import forms
-
+from django.forms import Form, ModelForm, DateField, widgets
 from .models import *
 
-
 class UserForm(forms.ModelForm):
+
 
     class Meta:
         model = User1
         fields = ['first_name', 'last_name', 'email', 'address', 'city', 'state', 'pincode', 'phone', "license_no",
                   "qualification", "dob", "certificate", "type"]
+
+        widgets = {
+            'dob': widgets.DateInput(attrs={'type': 'date'})
+        }
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
 from .forms import *
 
+
 # Create your views here.
 
 
 def register(request):
-    register_form = UserForm(request.POST)
+    if request.method == "POST":
+        user = User1.objects.create(first_name=request.POST["first_name"], last_name=request.POST["last_name"],
+                                    license_no=request.POST["license_no"],
+                                    address=request.POST["address"],
+                                    city=request.POST["city"],
+                                    state=request.POST["state"],
+                                    pincode=request.POST["pincode"],
+                                    phone=request.POST["phone"],
+                                    qualification=request.POST["qualification"],
+                                    dob=request.POST["dob"],
+                                    certificate=request.POST["certificate"],
+                                    type=request.POST["type"],
+                                    username=request.POST["license_no"],
+                                    )
+        user.save()
+
+    register_form = UserForm(request.POST, request.FILES)
     context = {'register_form': register_form}
     return render(request, 'register.html', context)
